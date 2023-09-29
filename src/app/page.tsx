07299159ba937b5642728936242e32ffb13c56d4
Aswath K <aswath.sana@gmail.com>
2023-09-29 17:43:23 +0530

    chore: Simple home page that refers to blog route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div className="container mx-auto h-screen flex flex-col justify-center items-center">
+      <h1 className="text-5xl">Aswath K</h1>
+      <a className="text-sky-500 text-center" href="/blog">
+        /blog
+      </a>
+    </div>
+  );
 }
