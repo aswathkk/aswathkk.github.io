afbe9206f24053631af623fd9372f2ee9200a8b0
Aswath K <aswath.sana@gmail.com>
2023-09-30 00:12:45 +0530

    chore: Restructure directories to match NextJS docs

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="container mx-auto h-screen flex flex-col justify-center items-center">
       <h1 className="text-5xl">Aswath K</h1>
-      <a className="text-sky-500 text-center" href="/blog">
-        /blog
-      </a>
+      <Link href="/blog">
+        <span className="text-sky-500 text-center">/blog</span>
+      </Link>
     </div>
   );
 }
